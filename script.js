76963c2b0c66500d75b84657d1188d6b699f40db
Rajdeep <rsinghk25@ilb.cat>
2026-03-30 20:30:18 +0200

    creacio dels noms

---
+++ script.js
@@ -0,0 +1,8 @@
+var formulari = document.getElementById('formulariAlumne');
+var inputNom = document.getElementById('nom');
+var inputExamen = document.getElementById('examen');
+var inputPractiques = document.getElementById('practiques');
+var inputActitud = document.getElementById('actitud');
+var missatge = document.getElementById('missatge');
+var cosTaula = document.getElementById('cosTaula');
+ 
